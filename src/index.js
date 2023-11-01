f1167efff787439ab95e0e8f2896945bd738645a
Muni-M <111742416+Muniraja-M@users.noreply.github.com>
2023-11-01 14:07:29 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "react-query"; // Import from "react-query"
+
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
@@ -14,5 +15,5 @@ root.render(
     <Router>
       <App />
     </Router>
-  </QueryClientProvider>
+  </QueryClientProvider>,
 );
