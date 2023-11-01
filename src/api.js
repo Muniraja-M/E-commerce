32add6b8fe7eff43c223d546263f64075a99bc79
Muni-M <111742416+Muniraja-M@users.noreply.github.com>
2023-11-01 14:08:34 +0000

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,12 +1,15 @@
 // api.js
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "react-query";
 
+// Define a function to fetch the products
 export function useProducts() {
   return useQuery("products", async () => {
     const response = await fetch("https://fakestoreapi.com/products");
+
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
+
     return response.json();
   });
 }
