88637dbc3ea59c8706d180d46cdc1f8d5727ea4a
Muniraja-M <111742416+Muniraja-M@users.noreply.github.com>
2023-11-06 20:23:10 +0530

    small changes

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import '../styles.css';
 function ProductItem({ product, onAddToCart, isBrief }) {
     const navigate = useNavigate();
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { useProducts } from '../api';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../redux/cartslice';
+import Notification from '../components/Notification';
+
 
 function Product() {
     const { id } = useParams(); // Get the product ID from the URL
     const { data: products, isLoading, isError } = useProducts();
     // Find the selected product in the cart based on the product ID
+    const dispatch = useDispatch(); // Get the dispatch function
+    const [notification, setNotification] = useState(null);
     if (isLoading) {
         return <div>Loading...</div>;
     }
@@ -14,28 +20,49 @@ function Product() {
     if (isError) {
         return <div>Error fetching products</div>;
     }
-    console.log('need data--', products);
-    console.log('id--', useParams());
+  
 
     const selectedProduct = products?.find(
         (product) => product.id === Number(id)
     );
 
+    const addToCartHandler = (selectedProduct) => {
+        dispatch(addToCart(selectedProduct));
+        setNotification('Item added to the cart');
+        setTimeout(() => {
+            setNotification(null);
+        }, 3000);
+    };
+    console.log("222222",selectedProduct)
+
     return (
         <div
-            className='item-container'
-            style={{ margin: 'auto', padding: '10rem' }}
+            className='items-container'
+            style={{ }}
         >
-            <h1>{selectedProduct.title}</h1>
+            <h1 className='font'>Product Detail Page</h1><br/>
+            <h1>{selectedProduct.title}</h1><br/>
             <img
                 src={selectedProduct.image}
                 alt={selectedProduct.title}
                 className='grid-container'
-            />
-            <p>Price: ${selectedProduct.price}</p>
-            <p>Description: {selectedProduct.description}</p>
-            <p>Rating: {selectedProduct.rating?.rate}</p>
-            <p>Reviews: {selectedProduct.rating?.count}</p>
+            /><br/>
+            <p><b>Price:</b> ${selectedProduct.price}</p><br/>
+            <p><b>Description:</b> {selectedProduct.description}</p><br/>
+            <p><b>Rating:</b> {selectedProduct.rating?.rate}</p><br/>
+            <p><b>Reviews:</b> {selectedProduct.rating?.count}</p><br/>
+            <button
+                        className='addCart-button'
+                        onClick={() => addToCartHandler(selectedProduct)}
+                    >
+                        Add to Cart
+                    </button>
+                    {notification && (
+                <Notification
+                    message={notification}
+                    onClose={() => setNotification(null)}
+                />
+            )}
         </div>
     );
 }
