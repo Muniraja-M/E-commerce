d99f9c3d932a644a198a3739e72810184a064828
arjun prakash <arjunprakash@arjuns-MacBook-Air.local>
2023-11-13 22:26:02 +0530

    async thunk

--- src/redux/productslice.js
+++ src/redux/productslice.js
@@ -3,19 +3,22 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 // Async thunk for fetching products
-export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
-  try {
-    const response = await fetch('https://fakestoreapi.com/products');
-    if (response.ok) {
-      const data = await response.json();
-      return data; // Fulfilled action with payload
-    } else {
-      throw new Error('Network response was not ok');
+export const fetchProducts = createAsyncThunk(
+    'productsSlice/fetchProducts',
+    async () => {
+        try {
+            const response = await fetch('https://fakestoreapi.com/products');
+            if (response.ok) {
+                const data = await response.json();
+                return data; // Fulfilled action with payload
+            } else {
+                throw new Error('Network response was not ok');
+            }
+        } catch (error) {
+            throw new Error(error.message); // Rejected action with error payload
+        }
     }
-  } catch (error) {
-    throw new Error(error.message); // Rejected action with error payload
-  }
-});
+);
 
 const productsSlice = createSlice({
     name: 'products',
